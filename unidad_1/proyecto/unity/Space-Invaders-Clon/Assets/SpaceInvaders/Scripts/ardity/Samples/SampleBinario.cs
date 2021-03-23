9eaed10d0778721e04bd4dc1634f66de5d63fcc8
Edisson Penagos <edisson.penagos@upb.edu.co>
2021-03-22 22:25:36 -0500

    inclusion del proyecto de unity
    el proyecto de unity lo tenia por error en un submodulo que me toco eliminar para poder colocarlo aqui en el repositorio principal

---
+++ unidad_1/proyecto/unity/Space-Invaders-Clon/Assets/SpaceInvaders/Scripts/ardity/Samples/SampleBinario.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+
+public class SampleBinario : MonoBehaviour
+{
+    public SerialControllerBinario serialController;
+
+    // Initialization
+    void Start()
+    {
+        serialController = GameObject.Find("SerialController").GetComponent<SerialControllerBinario>();
+        
+    }
+
+    // Executed each frame
+    void Update()
+    {
+
+
+        //---------------------------------------------------------------------
+        // Receive data
+        //---------------------------------------------------------------------
+
+        byte[] message = serialController.ReadSerialMessage();
+
+        if (message == null)
+            return;
+        StringBuilder sb = new StringBuilder();
+        sb.Append("Packet: ");
+        foreach (byte data in message)
+        {
+            sb.Append(data.ToString("X2") + " ");
+        }
+        Debug.Log(sb);
+    }
+}
